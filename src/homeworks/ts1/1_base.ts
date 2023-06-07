82ab9949ecba765498c7252c588ab5d0df76cd91
a.lysyuk <a.lysyuk@xsola.com>
2023-06-07 23:05:06 +0500

    hw1

---
+++ src/homeworks/ts1/1_base.ts
@@ -0,0 +1,76 @@
+/**
+ * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
+ * */
+export const removePlus = (string: string) => string.replace(/^\+/, '');
+
+export const addPlus = (string: string) => `+${string}`;
+
+export const removeFirstZeros = (value: string) => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+
+export const getBeautifulNumber = (value: number, separator = ' '): string =>
+  value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+
+export const round = (value: number, accuracy = 2) => {
+  const d = 10 ** accuracy;
+  return Math.round(value * d) / d;
+};
+
+const transformRegexp =
+  /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
+
+export const getTransformFromCss = (transformCssString: string): { x: number; y: number } => {
+  const data = transformCssString.match(transformRegexp);
+  if (!data) return { x: 0, y: 0 };
+  return {
+    x: parseInt(data[6], 10),
+    y: parseInt(data[8], 10),
+  };
+};
+
+export const getColorContrastValue = (rgb: number[]): number => {
+  const [red, green, blue] = rgb;
+  return Math.round((red * 299 + green * 587 + blue * 114) / 1000);
+};
+
+export const getContrastType = (contrastValue: number): string => (contrastValue > 125 ? 'black' : 'white');
+
+export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
+export const longColorRegExp = /^#[0-9a-f]{6}$/i;
+
+export const checkColor = (color: string): void => {
+  if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
+};
+
+export const hex2rgb = (color: string): number[] => {
+  checkColor(color);
+  if (shortColorRegExp.test(color)) {
+    const red = parseInt(color.substring(1, 2), 16);
+    const green = parseInt(color.substring(2, 3), 16);
+    const blue = parseInt(color.substring(3, 4), 16);
+    return [red, green, blue];
+  }
+  const red = parseInt(color.substring(1, 3), 16);
+  const green = parseInt(color.substring(3, 5), 16);
+  const blue = parseInt(color.substring(5, 8), 16);
+  return [red, green, blue];
+};
+
+export const getNumberedArray = (arr: string[]): { value: string; number: number }[] =>
+  arr.map((value: string, number: number) => ({ value, number }));
+export const toStringArray = (arr: { value: string; number: number }[]): string[] =>
+  arr.map(({ value, number }) => `${value}_${number}`);
+
+export const transformCustomers = (
+  customers: { id: number; name: string; age: number; isSubscribed: boolean }[]
+): { [id: number]: { name: string; age: number; isSubscribed: boolean } } => {
+  return customers.reduce(
+    (
+      acc: { [x: string]: { name: string; age: number; isSubscribed: boolean } },
+      customer: { id: number; name: string; age: number; isSubscribed: boolean }
+    ) => {
+      acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
+      return acc;
+    },
+    {}
+  );
+};
--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -8,8 +8,13 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
- *
- * Продукт (Product) содержит
+ */
+type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+/* Продукт (Product) содержит
  * - id (строка)
  * - name (строка)
  * - photo (строка)
@@ -18,10 +23,21 @@
  * - oldPrice (число, необязательно)
  * - price (строка)
  * - category (Категория)
- *
- * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
- *
- * Трата (Cost) содержит
+ */
+type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: string;
+  category: Category;
+};
+/* Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
+ */
+type Operation = Cost | Profit;
+/* Трата (Cost) содержит
  * - id (строка)
  * - name (строка)
  * - desc (строка, необязательно)
@@ -29,8 +45,17 @@
  * - amount (число)
  * - category (Категория)
  * - type ('Cost')
- *
- * Доход (Profit) содержит
+ */
+type Cost = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Cost';
+};
+/* Доход (Profit) содержит
  * - id (строка)
  * - name (строка)
  * - desc (строка, необязательно)
@@ -39,15 +64,61 @@
  * - category (Категория)
  * - type ('Profit')
  * */
-
+type Profit = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: Category;
+  type: 'Profit';
+};
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+  const productName = 'Example Product';
+  const productId = '1';
+  const productPhoto = 'https://productphoto.com/photo.jpg';
+  const productDesc = '';
+  const oldProdPrice = 100;
+  const productPrice = '50$';
+  const productCategory: Category = {
+    id: '2',
+    name: 'Category example',
+  };
+
+  const product: Product = {
+    id: productId,
+    name: productName,
+    createdAt: createdAt,
+    photo: productPhoto,
+    desc: productDesc,
+    oldPrice: oldProdPrice,
+    price: productPrice,
+    category: productCategory,
+  };
+  return product;
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  const productCategory: Category = {
+    id: '2',
+    name: 'Category example',
+  };
+  const cost: Cost = {
+    id: '1',
+    name: 'cost name',
+    createdAt: createdAt,
+    amount: 3,
+    category: productCategory,
+    type: 'Cost',
+  };
+  const operation: Operation = cost;
+  return operation;
+};
